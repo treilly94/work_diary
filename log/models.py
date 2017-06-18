826139ec8095637500e0b95f0f711fbe8384b78d
Tom Reilly <treilly94@live.com>
2017-06-18 19:38:14 +0100

    made print exp

--- log/models.py
+++ log/models.py
@@ -1,6 +1,10 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 class Exp(models.Model):
+    user = models.CharField(max_length=100)
     field = models.CharField(max_length=200)
     description = models.TextField(max_length=500, default='', blank=True)
     date = models.DateTimeField('date')
--- log/views.py
+++ log/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Exp
+
 
 
 def index(request):
-    return HttpResponse('hello')
+    latest_exp_list = Exp.objects.order_by('date')[:5]
+    context = {
+        'latest_exp_list': latest_exp_list,
+    }
+    return render(request, 'log/index.html', context)
