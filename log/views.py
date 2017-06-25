ac97f4885cfb51226805c92beae9294ba58cd328
kayleigh94 <kayleigh.bellis@hotmail.co.uk>
2017-06-25 20:06:54 +0100

    adding tom page

--- log/views.py
+++ log/views.py
@@ -11,3 +11,9 @@ def index(request):
         'latest_exp_list': latest_exp_list,
     }
     return render(request, 'log/index.html', context)
+def tom(request):
+    latest_exp_list = Exp.objects.order_by('date')[:5]
+    context = {
+        'latest_exp_list': latest_exp_list,
+    }
+    return render(request, 'log/tom.html', context)
