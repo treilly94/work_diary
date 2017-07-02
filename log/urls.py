3d83d3ced40c40dbd688e88ce71cff601720d207
Tom Reilly <treilly94@live.com>
2017-07-02 16:02:49 +0100

    Updated to use generic views

--- log/urls.py
+++ log/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^$', views.tom, name='tom'),
-	url(r'^$', views.base, name='base'), 
+	url(r'^$', views.base, name='base'),
 ]
--- log/views.py
+++ log/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Exp
 
 
 
-def index(request):
-    latest_exp_list = Exp.objects.order_by('date')[:5]
-    context = {
-        'latest_exp_list': latest_exp_list,
-    }
-    return render(request, 'log/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'log/index.html'
+    context_object_name = 'latest_exp_list'
+
+    def get_queryset(self):
+        """
+        Return the all Exp alphabetically
+        """
+        return Exp.objects.order_by('date')
+
 def tom(request):
     latest_exp_list = Exp.objects.order_by('date')[:5]
     context = {
