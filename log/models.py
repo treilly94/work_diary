3b55f8b0e9eb68ec924f2fd03a5872f645a632a1
Tom Reilly <treilly94@live.com>
2017-07-03 18:41:44 +0100

    Made date time deafault to now

--- log/models.py
+++ log/models.py
@@ -7,7 +7,8 @@ class Exp(models.Model):
     user = models.CharField(max_length=100)
     field = models.CharField(max_length=200)
     description = models.TextField(max_length=500, default='', blank=True)
-    date = models.DateTimeField('date')
+    
+    date = models.DateTimeField('date', default=timezone.now)
 
     def __str__(self):
         return self.field
