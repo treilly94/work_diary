1c83bc7d33522d2ee1ede6a9a736ce2fff9a0f81
Tom Reilly <treilly94@live.com>
2017-07-03 19:00:10 +0100

    updated database

--- log/admin.py
+++ log/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Exp
 
+class ExpAdmin(admin.ModelAdmin):
+    list_display = ('date', 'user', 'field')
+    list_filter = ['user', 'field']
+    search_fields = ['user', 'field']
+
 admin.site.register(Exp)
--- log/models.py
+++ log/models.py
@@ -7,7 +7,7 @@ class Exp(models.Model):
     user = models.CharField(max_length=100)
     field = models.CharField(max_length=200)
     description = models.TextField(max_length=500, default='', blank=True)
-    link = models.URLField(blank=True)
+    link = models.URLField(max_length=200, default='', blank=True)
     date = models.DateTimeField('date', default=timezone.now)
 
     def __str__(self):
