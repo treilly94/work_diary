3d20e775201e60eb3471f948e14ce8193c6f409f
kayleigh94 <kayleigh.bellis@hotmail.co.uk>
2017-07-11 20:05:41 +0100

    turning it inot a button

--- log/templates/javascripts/java_overlay.js
+++ log/templates/javascripts/java_overlay.js
@@ -1,6 +1,4 @@
-function openBlogCreation() {
-    document.getElementById("newBlog").style.display = "block";
-}
-function closeBlogCreation() {
-    document.getElementById("newBlog").style.display = "none";
+document.getElementById("createblog_button").onclick = function () {
+        location.href = "{% url 'createblog' %}";
+    };
 }
