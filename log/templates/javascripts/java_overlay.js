389f89c6b26c1838353c5323ffc2d33b321b9477
kayleigh94 <kayleigh.bellis@hotmail.co.uk>
2017-07-11 19:39:22 +0100

    adding overlay

---
+++ log/templates/javascripts/java_overlay.js
@@ -0,0 +1,6 @@
+function openBlogCreation() {
+    document.getElementById("newBlog").style.height = "100%";
+}
+function closeBlogCreation() {
+    document.getElementById("newBlog").style.height = "0%";
+}
